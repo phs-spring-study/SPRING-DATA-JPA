fa9d88b05a7aa7bafb3268329cc80d0a3971fce2
ksi0921 <ksi0921@midasit.com>
2022-01-19 00:38:49 +0900

    feat: 페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,11 +1,18 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,4 +29,19 @@ public class MemberController {
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
     }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> pageDto = page.map(MemberDto::new);
+        return pageDto;
+    }
+
+    @PostConstruct
+    public void init() {
+        for(int i=0; i< 100; i++) {
+            memberRepository.save(new Member("user" + i, i, null));
+        }
+    }
+
 }
--- data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
+++ data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
@@ -3,11 +3,19 @@ package study.datajpa.dto;
 import lombok.Data;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
+
 @Data
 public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    public MemberDto(Member m) {
+        this.id = m.getId();
+        this.username = m.getUsername();
+    }
+
     public MemberDto(Long id, String username, String teamName) {
         this.id = id;
         this.username = username;
